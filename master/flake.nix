a09a5980bf2c3358b2012c5abf9109493e83c067
hugosenari <hugosenari@gmail.com>
2022-03-12 04:07:22 -0300

    chore: re index shorturl

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Nim module for generating URL identifiers for Tiny URL and bit.ly-like URLs'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-shorturl-master.flake = false;
+  inputs.src-shorturl-master.owner = "achesak";
+  inputs.src-shorturl-master.ref   = "refs/heads/master";
+  inputs.src-shorturl-master.repo  = "nim-shorturl";
+  inputs.src-shorturl-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-shorturl-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-shorturl-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
